567942bbea5c2e8248bfc8ec748e4df311637c86
benjamin lew <bennru33@gmail.com>
2016-10-12 20:40:43 -0700

    adding a semicolon

--- public/main.js
+++ public/main.js
@@ -1,53 +1,58 @@
 //google maps api key : AIzaSyCb-7dyOecp8Y_ZQsqqY6PY6fbLLEc6XCk
 
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBAbgpJgr0A09hZRgK6qJgW-2i4rKjokDY",
-    authDomain: "map-poi-api.firebaseapp.com",
-    databaseURL: "https://map-poi-api.firebaseio.com",
-    storageBucket: "gs://map-poi-api.appspot.com",
-    messagingSenderId: "108082024426"
-  };
+var config = {
+  apiKey: "AIzaSyBAbgpJgr0A09hZRgK6qJgW-2i4rKjokDY",
+  authDomain: "map-poi-api.firebaseapp.com",
+  databaseURL: "https://map-poi-api.firebaseio.com",
+  storageBucket: "gs://map-poi-api.appspot.com",
+  messagingSenderId: "108082024426"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  // function initMap() {
-  //   var uluru = {lat: -25.363, lng: 131.044};
-  //   var map = new google.maps.Map(document.getElementById('map'), {
-  //     zoom: 4,
-  //     center: uluru
-  //   });
-  //   var marker = new google.maps.Marker({
-  //     position: uluru,
-  //     map: map
-  //   });
-  // }
+// function initMap() {
+//   var uluru = {lat: -25.363, lng: 131.044};
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: uluru
+//   });
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map
+//   });
+// }
 
 function initMap() {
- var map = new google.maps.Map(document.getElementById('map'), {
-   center: {lat: -34.397, lng: 150.644},
-   zoom: 6
- });
- var infoWindow = new google.maps.InfoWindow({map: map});
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 6
+  });
+  var infoWindow = new google.maps.InfoWindow({
+    map: map
+  });
 
- // Try HTML5 geolocation.
- if (navigator.geolocation) {
-   navigator.geolocation.getCurrentPosition(function(position) {
-     var pos = {
-       lat: position.coords.latitude,
-       lng: position.coords.longitude
-     };
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-     infoWindow.setPosition(pos);
-     infoWindow.setContent('Location found.');
-     map.setCenter(pos);
-   }, function() {
-     handleLocationError(true, infoWindow, map.getCenter());
-   });
- } else {
-   // Browser doesn't support Geolocation
-   handleLocationError(false, infoWindow, map.getCenter());
- }
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
   // Create a <script> tag and set the USGS URL as the source.
   var script = document.createElement('script');
@@ -57,21 +62,20 @@ function initMap() {
   document.getElementsByTagName('head')[0].appendChild(script);
 }
 
-
 window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-          var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-        }
-      }
+  for (var i = 0; i < results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1], coords[0]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+};
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
- infoWindow.setPosition(pos);
- infoWindow.setContent(browserHasGeolocation ?
-                       'Error: The Geolocation service failed.' :
-                       'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
 }
